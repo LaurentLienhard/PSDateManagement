2963dacc4a7d7b33aeb65e355bdd626c13264547
Laurent LIENHARD <laurent.lienhard@outlook.com>
2019-07-31 15:25:55 +0200

    add Comment help in New-DMDate

--- Sources/Functions/Public/New-DMDate.ps1
+++ Sources/Functions/Public/New-DMDate.ps1
@@ -1,4 +1,46 @@
 function New-DMDate {
+    <#
+    .SYNOPSIS
+        Create new date object
+    .DESCRIPTION
+        Create a DATE object from the current date of the day or by giving a date range.
+        Methods can be applied on this object to perform treatments such as:
+            * Calculation of the number of working days between 2 dates
+            * Calculation of the number of days between 2 dates
+    .PARAMETER Date
+        The date that will be used to instantiate the DATE object
+    .PARAMETER StartDate
+        The start date of the interval to instantiate the DATE object
+    .PARAMETER EndDate
+        The end date of the interval to instantiate the DATE object
+    .EXAMPLE
+        PS > New-DMDate
+        FirstDay            LastDay             aDate
+        --------            -------             -----
+        01/01/0001 00:00:00 01/01/0001 00:00:00 31/07/2019 15:13:22
+
+        This example returns a DATE object based on today's date
+    .EXAMPLE
+        PS > New-DMDate -Date ((get-date).AddDays(-6))
+        FirstDay            LastDay             aDate
+        --------            -------             -----
+        01/01/0001 00:00:00 01/01/0001 00:00:00 25/07/2019 15:15:12
+
+        This example returns a DATE object based on today's date minus 6 days
+    .EXAMPLE
+        PS > New-DMDate -StartDate ((get-date).AddDays(-6)) -EndDate ((get-date))
+        FirstDay            LastDay             aDate
+        --------            -------             -----
+        25/07/2019 15:16:43 31/07/2019 15:16:43 25/07/2019 15:16:43
+
+        This example returns a DATE object taking StartDate as the first day and EndDate as the last day.
+    .INPUTS
+        Inputs (if any)
+    .OUTPUTS
+        Output (if any)
+    .NOTES
+        General notes
+    #>
     [CmdletBinding(DefaultParameterSetName = "ByDay")]
     param (
         [Parameter(ParameterSetName = "ByDay")]
